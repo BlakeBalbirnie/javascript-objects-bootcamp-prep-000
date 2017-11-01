b5eeaf455e7540f6d7ae666b52069d2a821d00f2
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-11-01 16:05:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(obj, key, value) {
   return Object.assign({}, obj, { [key]: value})
 }
 
-function removeFromPlaylist(obj, key) {
+function removeFromPlaylist(obj, key, value) {
   delete playlist.artistName;
 }
